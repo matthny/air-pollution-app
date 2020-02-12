4c2686b2fee69a3f1b3cd373f993551e1e17b006
Maciej <>
2020-02-12 23:40:22 +0100

    closing drawer on click on handsets

--- src/app/components/navigation/navigation.component.ts
+++ src/app/components/navigation/navigation.component.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { map, first } from 'rxjs/operators';
+import { MatSidenav } from '@angular/material';
 
 @Component({
   selector: 'app-navigation',
@@ -9,6 +10,8 @@ import { map } from 'rxjs/operators';
   styleUrls: ['./navigation.component.css']
 })
 export class NavigationComponent {
+  @ViewChild('drawer')
+  private drawer: MatSidenav;
 
   isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
     .pipe(
@@ -17,4 +20,13 @@ export class NavigationComponent {
 
   constructor(private breakpointObserver: BreakpointObserver) {}
 
+    private handleClick(): void {
+      this.isHandset$
+        .pipe(first())
+        .subscribe((isHandset: boolean) => {
+          if (isHandset) {
+            this.drawer.close();
+          }
+        });
+    }
 }
