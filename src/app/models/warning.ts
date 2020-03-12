a6f7034b8ab31e6affc460840e1b6930b3c9fc85
Maciej <>
2020-03-12 22:46:07 +0100

    remove unused code

--- src/app/models/warning.ts
+++ src/app/models/warning.ts
@@ -8,38 +8,4 @@ export class Warning {
     this.parameter = parameter;
     this.warning = warning;
   }
-
-  private getWarningMessage(): string {
-    if (this.warning === PollutionCategory.Good) {
-      return 'Good';
-    }
-
-    if (this.warning === PollutionCategory.Moderate) {
-      return 'Moderate';
-    }
-
-    if (this.warning === PollutionCategory.UnhealthySensitive) {
-      return 'Unhealthy for Sensitive Groups';
-    }
-
-    if (this.warning === PollutionCategory.Unhealthy) {
-      return 'Unhealthy';
-    }
-
-    if (this.warning === PollutionCategory.VeryUnhealthy) {
-      return 'Very unhealthy';
-    }
-
-    if (this.warning === PollutionCategory.Hazardous) {
-      return 'Hazardous';
-    }
-
-    if (this.warning === PollutionCategory.VeryHazardous) {
-      return 'Very hazardous';
-    }
-
-    if (this.warning === PollutionCategory.OutOfRange) {
-      return 'Out of range';
-    }
-  }
 }
